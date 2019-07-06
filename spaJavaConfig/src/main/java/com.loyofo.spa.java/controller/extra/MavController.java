aedf296e7b481a77910bf046f3237e6a945880fb
luojbin <z1_2x@msn.com>
2019-07-06 21:46:33 +0800

    mav 测试

--- spaJavaConfig/src/main/java/com.loyofo.spa.java/controller/extra/MavController.java
+++ spaJavaConfig/src/main/java/com.loyofo.spa.java/controller/extra/MavController.java
@@ -5,12 +5,21 @@ import com.loyofo.spa.java.entity.Spittle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 /**
- * model and view 不同情况的附加测试
+ * model and view 不同情况的附加测试, 有以下几种类型
+ *
+ * 1. model 入参, 返回视图名
+ * 2. model 入参, 不返回视图名
+ * 3. map 入参, 返回视图名
+ * 4. map 入参, 不返回视图名
+ * 5. 无入参, 直接返会数据
+ * 6. 无入参, 返回 ModelAndView
  */
 @RequestMapping("/mav")
 public class MavController {
@@ -25,8 +34,8 @@ public class MavController {
      * 获取 Model 为参数, 可以通过这个 model 将数据传递给视图, 方法返回视图名
      * 不指定数据对象的名字, 则根据类型推断, 此处推断为 stringList
      */
-    @RequestMapping("msgAndModel")
-    public String getMsgAndModel(Model model) {
+    @RequestMapping("modelAndView")
+    public String modelAndView(Model model) {
         List<Spittle> list = spittleRepository.findSpittles(Long.MAX_VALUE, 20);
         model.addAttribute(list);
         return "msg";
@@ -35,30 +44,48 @@ public class MavController {
     /**
      * 获取 Model 为参数, 可以通过这个 model 将数据传递给视图, 方法返回视图名
      */
-    @RequestMapping("msgAndModelWithName")
-    public String getMsgAndModelWithName(Model model) {
+    @RequestMapping("modelNoView")
+    public Model modelNoView(Model model) {
         List<Spittle> list = spittleRepository.findSpittles(Long.MAX_VALUE, 20);
         model.addAttribute("modelWithName", list);
-        return "msg";
+        return model;
     }
 
     /**
      * 获取 Map 作为参数, 也可以将数据传递给视图, 方法返回视图名
      */
-    @RequestMapping("msgAndMap")
-    public String getMsgAndMap(Map<String, Object> map) {
+    @RequestMapping("mapAndView")
+    public String mapAndView(Map<String, Object> map) {
         List<Spittle> list = spittleRepository.findSpittles(Long.MAX_VALUE, 20);
-        map.put("msgAndMap", list);
+        map.put("mapAndView", list);
         return "msg";
+    }
+    /**
+     * 获取 Map 作为参数, 也可以将数据传递给视图, 方法返回视图名
+     */
+    @RequestMapping("mapNoView")
+    public Map<String, Object> mapNoView(Map<String, Object> map) {
+        List<Spittle> list = spittleRepository.findSpittles(Long.MAX_VALUE, 20);
+        map.put("mapNoView", list);
+        return map;
     }
 
     /**
      * 直接返回对象, 则这个对象会被放到 request 中传递给视图, key 根据对象的类型推断, 此处推断key 为 stringList
      * 不指定视图名, 则视图名等于请求名
      */
-    @RequestMapping("msg")
-    public List<Spittle> getMsg() {
+    @RequestMapping("dataNoView")
+    public List<Spittle> data() {
         return spittleRepository.findSpittles(Long.MAX_VALUE, 20);
     }
+
+    @RequestMapping("mavObject")
+    public ModelAndView mavObject() {
+        Map<String, Object> data = new HashMap<>();
+        data.put("mavData", spittleRepository.findSpittles(Long.MAX_VALUE, 20));
+        return new ModelAndView("mavView", data);
+    }
+
+
 
 }
--- spaJavaConfig/src/test/java/com/loyofo/spa/test/controller/MavControllerTest.java
+++ spaJavaConfig/src/test/java/com/loyofo/spa/test/controller/MavControllerTest.java
@@ -1,11 +1,9 @@
 package com.loyofo.spa.test.controller;
 
-import com.loyofo.spa.java.controller.HomeController;
 import com.loyofo.spa.java.controller.extra.MavController;
 import com.loyofo.spa.java.dao.SpittleRepository;
 import com.loyofo.spa.java.entity.Spittle;
 import org.hamcrest.Matchers;
-import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mockito;
 import org.springframework.test.web.servlet.MockMvc;
@@ -21,39 +19,37 @@ import java.util.List;
 public class MavControllerTest {
 
     @Test
-    public void testGetMsgAndModel() throws Exception {
+    public void testModelAndView() throws Exception {
         List<Spittle> expectedMsg = getSpittleList(20);
         SpittleRepository mockRepository = Mockito.mock(SpittleRepository.class);
         Mockito.when(mockRepository.findSpittles(Long.MAX_VALUE, 20)).thenReturn(expectedMsg);
 
         MavController controller = new MavController(mockRepository);
         MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller)
-                .setSingleView(new InternalResourceView("/WEB-INF/views/msg.jsp"))
                 .build();
-        mockMvc.perform(MockMvcRequestBuilders.get("/msgAndModel"))
+        mockMvc.perform(MockMvcRequestBuilders.get("/mav/modelAndView"))
                 .andExpect(MockMvcResultMatchers.view().name("msg"))
-                .andExpect(MockMvcResultMatchers.model().attributeExists("stringList"))
-                .andExpect(MockMvcResultMatchers.model().attribute("stringList", Matchers.hasItems(expectedMsg.toArray())));
-
+                .andExpect(MockMvcResultMatchers.model().attributeExists("spittleList"))
+                .andExpect(MockMvcResultMatchers.model().attribute("spittleList", Matchers.hasItems(expectedMsg.toArray())));
     }
     @Test
-    public void testGetMsgAndModelWithName() throws Exception {
+    public void testModelNoView() throws Exception {
         List<Spittle> expectedMsg = getSpittleList(20);
         SpittleRepository mockRepository = Mockito.mock(SpittleRepository.class);
         Mockito.when(mockRepository.findSpittles(Long.MAX_VALUE, 20)).thenReturn(expectedMsg);
 
         MavController controller = new MavController(mockRepository);
         MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller)
-                .setSingleView(new InternalResourceView("/WEB-INF/views/msg.jsp"))
+                .setSingleView(new InternalResourceView("/WEB-INF/views/modelNoView.jsp"))
                 .build();
-        mockMvc.perform(MockMvcRequestBuilders.get("/msgAndModelWithName"))
-                .andExpect(MockMvcResultMatchers.view().name("msg"))
+        mockMvc.perform(MockMvcRequestBuilders.get("/mav/modelNoView"))
+                .andExpect(MockMvcResultMatchers.view().name("mav/modelNoView"))
                 .andExpect(MockMvcResultMatchers.model().attributeExists("modelWithName"))
                 .andExpect(MockMvcResultMatchers.model().attribute("modelWithName", Matchers.hasItems(expectedMsg.toArray())));
 
     }
     @Test
-    public void testGetMsgAndMap() throws Exception {
+    public void testMapAndView() throws Exception {
         List<Spittle> expectedMsg = getSpittleList(20);
         SpittleRepository mockRepository = Mockito.mock(SpittleRepository.class);
         Mockito.when(mockRepository.findSpittles(Long.MAX_VALUE, 20)).thenReturn(expectedMsg);
@@ -62,14 +58,14 @@ public class MavControllerTest {
         MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller)
                 .setSingleView(new InternalResourceView("/WEB-INF/views/msg.jsp"))
                 .build();
-        mockMvc.perform(MockMvcRequestBuilders.get("/msgAndMap"))
+        mockMvc.perform(MockMvcRequestBuilders.get("/mav/mapAndView"))
                 .andExpect(MockMvcResultMatchers.view().name("msg"))
-                .andExpect(MockMvcResultMatchers.model().attributeExists("msgAndMap"))
-                .andExpect(MockMvcResultMatchers.model().attribute("msgAndMap", Matchers.hasItems(expectedMsg.toArray())));
+                .andExpect(MockMvcResultMatchers.model().attributeExists("mapAndView"))
+                .andExpect(MockMvcResultMatchers.model().attribute("mapAndView", Matchers.hasItems(expectedMsg.toArray())));
 
     }
     @Test
-    public void testGetMsg() throws Exception {
+    public void testMapNoView() throws Exception {
         List<Spittle> expectedMsg = getSpittleList(20);
         SpittleRepository mockRepository = Mockito.mock(SpittleRepository.class);
         Mockito.when(mockRepository.findSpittles(Long.MAX_VALUE, 20)).thenReturn(expectedMsg);
@@ -78,11 +74,40 @@ public class MavControllerTest {
         MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller)
                 .setSingleView(new InternalResourceView("/WEB-INF/views/msg.jsp"))
                 .build();
-        mockMvc.perform(MockMvcRequestBuilders.get("/msg"))
-                .andExpect(MockMvcResultMatchers.view().name("msg"))
-                .andExpect(MockMvcResultMatchers.model().attributeExists("stringList"))
-                .andExpect(MockMvcResultMatchers.model().attribute("stringList", Matchers.hasItems(expectedMsg.toArray())));
+        mockMvc.perform(MockMvcRequestBuilders.get("/mav/mapNoView"))
+                .andExpect(MockMvcResultMatchers.view().name("mav/mapNoView"))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("mapNoView"))
+                .andExpect(MockMvcResultMatchers.model().attribute("mapNoView", Matchers.hasItems(expectedMsg.toArray())));
+    }
+    @Test
+    public void testDataNoView() throws Exception {
+        List<Spittle> expectedMsg = getSpittleList(20);
+        SpittleRepository mockRepository = Mockito.mock(SpittleRepository.class);
+        Mockito.when(mockRepository.findSpittles(Long.MAX_VALUE, 20)).thenReturn(expectedMsg);
 
+        MavController controller = new MavController(mockRepository);
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller)
+                .setSingleView(new InternalResourceView("/WEB-INF/views/msg.jsp"))
+                .build();
+        mockMvc.perform(MockMvcRequestBuilders.get("/mav/dataNoView"))
+                .andExpect(MockMvcResultMatchers.view().name("mav/dataNoView"))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("spittleList"))
+                .andExpect(MockMvcResultMatchers.model().attribute("spittleList", Matchers.hasItems(expectedMsg.toArray())));
+    }
+    @Test
+    public void testMavObject() throws Exception {
+        List<Spittle> expectedMsg = getSpittleList(20);
+        SpittleRepository mockRepository = Mockito.mock(SpittleRepository.class);
+        Mockito.when(mockRepository.findSpittles(Long.MAX_VALUE, 20)).thenReturn(expectedMsg);
+
+        MavController controller = new MavController(mockRepository);
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller)
+                .setSingleView(new InternalResourceView("/WEB-INF/views/msg.jsp"))
+                .build();
+        mockMvc.perform(MockMvcRequestBuilders.get("/mav/mavObject"))
+                .andExpect(MockMvcResultMatchers.view().name("mavView"))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("mavData"))
+                .andExpect(MockMvcResultMatchers.model().attribute("mavData", Matchers.hasItems(expectedMsg.toArray())));
     }
 
     private List<Spittle> getSpittleList(int count) {
