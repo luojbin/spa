3d10211931b1439c4a0a263b854532d3e83ce2c7
luojbin <z1_2x@msn.com>
2019-08-08 18:15:52 +0800

    stub 方法

--- spaJavaConfig/src/test/java/com/loyofo/spa/test/mockito/EmptyMethodTest.java
+++ spaJavaConfig/src/test/java/com/loyofo/spa/test/mockito/EmptyMethodTest.java
@@ -4,10 +4,8 @@ import com.loyofo.spa.test.mockito.entity.MockInterface;
 import com.loyofo.spa.test.mockito.entity.MockObject;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 public class EmptyMethodTest {
 
@@ -16,5 +14,29 @@ public class EmptyMethodTest {
     public void testEmptyReturn() {
         MockInterface mockByInterface = mock(MockInterface.class);
         assertNull(mockByInterface.strMethod("mockByInterface 调用 String 方法"));
+        assertEquals(mockByInterface.intMethod(1), 0);
+        assertFalse(mockByInterface.booleanMethod(false));
+        assertFalse(mockByInterface.booleanMethod(true));
+    }
+    
+    @Test
+    public void testStubMethod() {
+        MockObject mockObject = mock(MockObject.class);
+        String param = "指定入参";
+        String return1 = "返回值-1";
+        String return2 = "返回值-2";
+        String return3 = "返回值-3";
+        when(mockObject.strMethod(param))
+                .thenReturn(return1)
+                .thenReturn(return2)
+                .thenReturn(return3);
+
+        assertEquals(mockObject.strMethod(param), return1);
+        assertEquals(mockObject.strMethod(param), return2);
+        assertEquals(mockObject.strMethod(param), return3);
+        assertEquals(mockObject.strMethod(param), return3);
+        assertEquals(mockObject.strMethod(param), return3);
+        assertEquals(mockObject.strMethod(param), return3);
+        assertEquals(mockObject.strMethod(param), return3);
     }
 }
