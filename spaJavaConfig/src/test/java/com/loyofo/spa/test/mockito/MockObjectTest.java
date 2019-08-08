34afdcfb1973ccfc469b6c65573f38aa103724ab
luojbin <z1_2x@msn.com>
2019-08-08 17:45:49 +0800

    构造模拟对象

--- spaJavaConfig/src/test/java/com/loyofo/spa/test/mockito/MockObjectTest.java
+++ spaJavaConfig/src/test/java/com/loyofo/spa/test/mockito/MockObjectTest.java
@@ -16,53 +16,31 @@ public class MockObjectTest {
     }
 
     @Test
-    public void testMockByInterface() {
-        // mock() 方法可以通过 interface 构造模拟对象
+    public void testMock() {
+        // mock() 方法可以通过 interface/class 构造模拟对象
         MockInterface mockByInterface = mock(MockInterface.class);
+        MockObject mockByClass = mock(MockObject.class);
 
         // 行为与普通的 MockInterface 实例一致, 可作实参, 可调方法
         MockInterface.useInstance(mockByInterface);
-        mockByInterface.strMethod("mockByInterface 调用 String 方法");
-    }
-
-    @Test
-    public void testMockByClass() {
-        // mock() 方法可以通过普通 class 构造模拟对象
-        MockObject mockByClass = mock(MockObject.class);
-
-        // 行为与普通的 MockObject 实例一致, 可作实参, 可调方法
         MockInterface.useInstance(mockByClass);
+        mockByInterface.strMethod("mockByInterface 调用 String 方法");
         mockByClass.strMethod("mockByClass 调用 String 方法");
     }
 
     @Test
-    public void testSpyByInterface() {
-        // spy() 方法可以通过 interface 构造模拟对象
+    public void testSpys() {
+        // spy() 方法可以通过 interface/class/已有对象 构造模拟对象
         MockInterface spyByInterface = spy(MockInterface.class);
+        MockObject spyByClass = spy(MockObject.class);
+        MockObject spyByRealObj = spy(new MockObject());
 
         // 行为与普通的 MockInterface 实例一致, 可作实参, 可调方法
         MockInterface.useInstance(spyByInterface);
-        spyByInterface.strMethod("spyByInterface 调用 String 方法");
-    }
-
-    @Test
-    public void testSpyByClass() {
-        // spy() 方法可以通过普通 class 构造模拟对象
-        MockObject spyByClass = spy(MockObject.class);
-
-        // 行为与普通的 MockInterface 实例一致, 可作实参, 可调方法
         MockInterface.useInstance(spyByClass);
-        spyByClass.strMethod("spyByClass 调用 String 方法");
-    }
-
-    @Test
-    public void testSpyByRealObj() {
-        // spy() 方法可以通过真实对象构造模拟对象, 相当于包装类
-        MockObject realObj = new MockObject();
-        MockObject spyByRealObj = spy(realObj);
-
-        // 行为与普通的 MockInterface 实例一致, 可作实参, 可调方法
         MockInterface.useInstance(spyByRealObj);
+        spyByInterface.strMethod("spyByInterface 调用 String 方法");
+        spyByClass.strMethod("spyByClass 调用 String 方法");
         spyByRealObj.strMethod("spyByRealObj 调用 String 方法");
     }
 }
