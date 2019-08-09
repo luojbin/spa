e833b92c6815ed587c437a34583b6bae70c600d1
luojbin <z1_2x@msn.com>
2019-08-09 11:15:57 +0800

    stub 方法

--- spaJavaConfig/src/test/java/com/loyofo/spa/test/mockito/EmptyMethodTest.java
+++ spaJavaConfig/src/test/java/com/loyofo/spa/test/mockito/EmptyMethodTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
 
 public class EmptyMethodTest {
 
@@ -26,17 +27,41 @@ public class EmptyMethodTest {
         String return1 = "返回值-1";
         String return2 = "返回值-2";
         String return3 = "返回值-3";
+        // 指定要模拟的方法调用
         when(mockObject.strMethod(param))
+                // 指定返回值, 重复调用时按给定顺序返回不同的值, 最后一个返回值会重复返回
                 .thenReturn(return1)
                 .thenReturn(return2)
                 .thenReturn(return3);
 
+        // 模拟调用
         assertEquals(mockObject.strMethod(param), return1);
         assertEquals(mockObject.strMethod(param), return2);
         assertEquals(mockObject.strMethod(param), return3);
         assertEquals(mockObject.strMethod(param), return3);
-        assertEquals(mockObject.strMethod(param), return3);
-        assertEquals(mockObject.strMethod(param), return3);
-        assertEquals(mockObject.strMethod(param), return3);
+    }
+
+    @Test
+    public void testWhen() {
+        String param = "指定入参";
+        String return1 = "返回值-1";
+
+        // 1.直接先调用一次, 能验证方法被调用过一次
+        MockObject mockObject = mock(MockObject.class);
+        mockObject.strMethod(param);
+        verify(mockObject, times(1)).strMethod(param);
+
+        // 2.模拟对象再次调用方法
+        mockObject.strMethod(param);
+        // 3.此时方法调用计数是2, 但只能在4以后代码被注释的情况下才能验证, 否则4中的 when(null) 会报错
+        // verify(mockObject, times(2)).strMethod(param);
+
+        // 4.紧跟在模拟对象方法调用后, 以 null 或任意 String 调用 when 方法, 都能正常模拟行为
+        when("任意参数").thenReturn(return1);
+        // 5.when 方法前的方法调用, 该次方法调用被撤销, 调用计数仍然为1
+        verify(mockObject, times(1)).strMethod(param);
+        // 6.再次调用后能获取指定的返回值, 调用计数+1
+        assertEquals(mockObject.strMethod(param), return1);
+        verify(mockObject, times(2)).strMethod(param);
     }
 }
