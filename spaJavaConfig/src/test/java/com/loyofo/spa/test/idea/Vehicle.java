00e2e64b2a515f18ad816885d2e2f943c8b232d6
luojbin <z1_2x@msn.com>
2019-09-23 10:20:59 +0800

    重构测试

--- spaJavaConfig/src/test/java/com/loyofo/spa/test/idea/Vehicle.java
+++ spaJavaConfig/src/test/java/com/loyofo/spa/test/idea/Vehicle.java
@@ -8,3 +8,16 @@ public abstract class Vehicle {
         System.out.println(name + "会跑");
     }
 }
+
+
+class MyClass {
+    public void myMethod(int value, double dd) {
+    }
+}
+
+class MyOtherClass {
+    public void myMethodCall(MyClass myClass) {
+        double d = 0.5;
+        myClass.myMethod(1, d);
+    }
+}
